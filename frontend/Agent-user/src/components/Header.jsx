0fc8f47f694d970d81fd9971b00433ac5e7f683e
Arief Mohammed <amohammed2@msa.miraclesoft.com>
2025-07-29 17:59:42 +0530

    second commit

--- frontend/Agent-user/src/components/Header.jsx
+++ frontend/Agent-user/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = ({ messageCount }) => {
+  console.log(messageCount)
   return (
     <div className=" bg-blue-300 p-4 shadow-lg">
       <h1 className="text-xl font-bold text-white">AI Assistant ✨</h1>
