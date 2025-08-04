b1889f533b3b5296136269e853014d93656c58b9
Arief Mohammed <amohammed2@msa.miraclesoft.com>
2025-08-04 15:02:18 +0530

    Intial push

--- frontend/Agent-user/src/components/Sidebar.jsx
+++ frontend/Agent-user/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <div className="w-64 bg-[#151527] border-r border-gray-800 p-4">
       <h2 className="text-xl font-semibold mb-4 text-white">Chat History</h2>
-      <button className="bg-gradient-to-r from-pink-500 to-purple-500 w-full py-2 rounded-md text-white font-medium">
+      <button className="light-blue w-full py-2 rounded-md text-white font-medium">
         + New Chat
       </button>
       <div className="mt-6 text-gray-400">
